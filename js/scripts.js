fe36b557c38392c273580c3a653f53314c71ebda
maiitieu <70587275+maiitieu@users.noreply.github.com>
2020-09-15 19:33:28 +0100

    Added code for nav scripts.js
    Added code for hamburger menu scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-console.log ("Hello, welcome to my portfolio site.");
+console.log ("Hello, welcome to my portfolio site.")
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
